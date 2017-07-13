11ead9de90c25dd6198fb48e5848bfadb8ce3964
Jeremy Bloomstrom <jeremy.bloomstrom@matsugov.us>
2017-07-13 15:06:05 -0800

    Append the new columns to the client output

--- app/Client.php
+++ app/Client.php
@@ -28,6 +28,11 @@ class Client extends Model
         'deleting' => ClientWasDestroyed::class,
     ];
 
+    protected $appends = [
+        'scanned_files_count',
+        'scanned_files_current'
+    ];
+
     /**
      * Set the name attribute
      * @method setNameAttribute
