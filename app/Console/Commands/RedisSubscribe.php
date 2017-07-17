815b6e08b42863fdce9d05c74f0fdec88d341abd
Jeremy Bloomstrom <jeremy.bloomstrom@matsugov.us>
2017-07-17 11:09:36 -0800

    Add redis subscriber handler

--- app/Console/Commands/RedisSubscribe.php
+++ app/Console/Commands/RedisSubscribe.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Client;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Redis;
 
@@ -39,7 +40,33 @@ class RedisSubscribe extends Command
     public function handle()
     {
         Redis::psubscribe(['*'],function($message, $channel) {
-            echo $channel . " " . $message;
+            return $this->route($message,$channel);
         });
+    }
+
+    /**
+     * Route the event
+     * @method route
+     *
+     * @return   void
+     */
+    private function route($message, $channel)
+    {
+        switch(true)
+        {
+            case preg_match("/clients\/(.*)\/heartbeat/", $channel, $matches) :
+                return $this->heartbeat($matches[1]);
+        }
+    }
+
+    /**
+     * Advance the heartbeat of the client
+     * @method heartbeat
+     *
+     * @return   void
+     */
+    private function heartbeat(Client $client)
+    {
+        $client->heartbeat();
     }
 }
